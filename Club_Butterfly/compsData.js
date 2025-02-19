af2659835d428b47b9a4bde1f8f6b9f6c1cf5d6f
Andy &theNIGHT <2spite@gmail.com>
2025-02-18 21:41:57 -0500

    far away and wag1

--- Club_Butterfly/compsData.js
+++ Club_Butterfly/compsData.js
@@ -35,5 +35,79 @@ const compsData = {
                 filePath: 'audio/clubButterfly/Club_Butterfly-BASS.wav',
             },
         ]
+    },
+    wag1: {
+        name: 'WAG1',
+        areas: [
+            {
+                x: 400,
+                y: 750,
+                h: 0,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/wag1/CH1.wav',
+            },
+            {
+                x: 750,
+                y: 400,
+                h: 25,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/wag1/CH2.wav',
+            },
+            {
+                x: 400,
+                y: 50,
+                h: 50,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/wag1/CH3.wav',
+            },
+            {
+                x: 50,
+                y: 400,
+                h: 75,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/wag1/CH4.wav',
+            },
+        ]
+    },
+    farAway: {
+        name: 'Far Away',
+        areas: [
+            {
+                x: 400,
+                y: 750,
+                h: 0,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/farAway/Far Away Pulp + HH.wav',
+            },
+            {
+                x: 750,
+                y: 400,
+                h: 25,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/farAway/Far Away Kick + Saw.wav',
+            },
+            {
+                x: 400,
+                y: 50,
+                h: 50,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/farAway/Far Away Bass + Rim.wav',
+            },
+            {
+                x: 50,
+                y: 400,
+                h: 75,
+                minRadius: 50,
+                maxRadius: 400,
+                filePath: 'audio/farAway/Far Away SNR + Bells.wav',
+            },
+        ]
     }
 }
